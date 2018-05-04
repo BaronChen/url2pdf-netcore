62925b1fb50716c13d8e6c68652733bf4fe234f0
baron <baron.zhongyangchen@gmail.com>
2018-05-05 00:14:01 +1000

    Change repo name

--- deploy/push-image.sh
+++ deploy/push-image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker tag url2pdfapi:latest baron123.azurecr.io/number-word/url2pdfapi:$1
+docker tag url2pdfapi:latest baron123.azurecr.io/url2pdf/url2pdfapi:$1
 docker login baron123.azurecr.io -u $DOCKER_USER -p $DOCKER_PASS        
-docker push baron123.azurecr.io/number-word/url2pdfapi
+docker push baron123.azurecr.io/url2pdf/url2pdfapi
 
